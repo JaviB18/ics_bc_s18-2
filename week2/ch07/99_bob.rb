3af9cdca3fd20447aae7786741fa102d50c2f182
JaviB18 <txrodrigues123@gmail.com>
2018-06-25 16:59:10 -0700

    Complete 99_bob exercise

--- week2/ch07/99_bob.rb
+++ week2/ch07/99_bob.rb
@@ -1,23 +1,25 @@
 greater = 99
-while greater != 1
+drink = 'Take one down and pass it around, '
+while greater != 2
   less = greater - 1
   bob = ' bottles of beer on the wall'
   verse1 = greater.to_s + bob + ', ' + greater.to_s + ' bottles of beer'
-  verse2 = 'Take one down and pass it around, ' + less.to_s + bob + '.' 
+  verse2 = drink + less.to_s + bob + '.'
   puts verse1
-  puts verse2 
+  puts verse2
   puts '  '
   greater = less
-  if greater == 1
-    break
-  end
 end
+one = '1 bottle of beer on the wall'
+plural = ' bottles of beer'
+puts '2' + plural + 'on the wall, 2' + plural
+puts drink + one + '.'
+puts ' '
 zero = 'no more bottles of beer on the wall'
 verse1 = zero.capitalize + ', ' + 'no more bottles of beer'
-verse2 = 'Take one down and pass it around, ' + zero + '.' 
-puts '1 bottle of beer on the wall, 1 bottle of beer.'
+verse2 = drink + zero + '.'
+puts one + ', 1 bottle of beer.'
 puts verse2
 puts ' '
 puts verse1
-puts 'Go to the store and buy some more, 99 bottles of beer on the wall' 
-
+puts 'Go to the store and buy some more, 99' + plural + ' on the wall.'
